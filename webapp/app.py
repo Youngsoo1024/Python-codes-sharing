d6bfa6c27a170bb819665ad174d7140021235f18
Youngsoo1024 <youngsoo.kim.1982@gmail.com>
2025-12-10 11:51:57 -0500

    update app.py on mac

--- webapp/app.py
+++ webapp/app.py
@@ -1,10 +1,18 @@
+
+# lower case for module names, upper case for class names
 from flask import Flask
+# dunder names is associate the webapp with your code's current namesapace.
 app = Flask(__name__)
+
 
 @app.get('/')
 def index():
+    # when invoked, this function, called "index", returns this exciting string
     return "This is a placeholder for your webapp's opening page."
 
+#This is the famous "dunder name equals dunder main" idiom. We've more to say about this "if" statement later.
+
 if __name__ == '__main__':
+    # This line of code runs Flask's built-in web server, then feeds your webapp code to it.
     app.run()
     
